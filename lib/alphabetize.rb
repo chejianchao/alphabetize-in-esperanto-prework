b994ba80a0a7971b9578c2bdb8b938e773b9c62c
chejianchao <820992376@qq.com>
2018-07-09 03:48:02 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,11 @@
 def alphabetize(arr)
   # code here
   ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  
+  ALPHA    = "ABabcdefghijklmnopqrstuvwxyz"
+  alpha_hash = {}
+  ALPHABET.each_with_index{|c, idx|
+    alpha_hash[c] = ALPHA[idx]
+  }
   arr.sort{|a,b|
     
   }
