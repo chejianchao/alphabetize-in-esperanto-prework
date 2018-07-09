827a6b5f865f005b19e57e440a4ccce722d4d265
chejianchao <820992376@qq.com>
2018-07-09 03:50:22 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -7,6 +7,18 @@ def alphabetize(arr)
     alpha_hash[c] = ALPHA[idx]
   }
   arr.sort{|a,b|
-    
+    a.each_with_idx{|c, idx|
+      if idx < b.size
+        if a[idx]<b[idx]
+          return 1
+        elsif a[idx] > b[idx]
+          return -1
+      end
+      if a.size == b.size
+        return 0
+      else
+        return -1
+      end
+    }
   }
 end
